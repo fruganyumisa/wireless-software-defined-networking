72a46710dbef62943f2249ce264704eb60ec995c
RugaCoder <fruganyumisa@gmail.com>
2019-09-21 12:04:48 -0400

    Managede manual reassociation

--- generic-wireless-topo.py
+++ generic-wireless-topo.py
@@ -11,21 +11,21 @@ from mn_wifi.cli import CLI_wifi
 def topology():
 
     print("Welcome to mininet-Wifi")
-    net = Mininet_wifi(controller = Controller, accessPoint =OVSKernelAP)
+    net = Mininet_wifi(controller = Controller, accessPoint =OVSKernelAP,allAutoAssociation=True)
 
     info("*** Creating nodes\n")
     ap1 = net.addAccessPoint('ap1', ssid= 'iGrid-ap1', mode = 'g', channel = '1',
-    position = '10,30,0', range = '20')
+    position = '10,30,0', range = '30')
     ap2 = net.addAccessPoint('ap2', ssid= 'iGrid-ap2', mode = 'g', channel = '6',
-    position = '50,30,0', range = '20')
+    position = '40,30,0', range = '20')
     sta1 = net.addStation('sta1', mac = '00:00:00:00:00:01', ip = '10.0.0.1/8',
-    position = '10,20,0')
+    position = '10,20,0',active_scan=1,scan_freq="2412 2437 2462")
     sta2 = net.addStation('sta2', mac = '00:00:00:00:00:02', ip = '10.0.0.2/8',
-    position = '50,20,0')
+    position = '28,30,0',active_scan=1,scan_freq="2412 2437 2462")
     sta3 = net.addStation('sta3', mac = '00:00:00:00:00:03', ip = '10.0.0.3/8',
-    position = '10,40,0')
+    position = '10,40,0',active_scan=1,scan_freq="2412 2437 2462")
     sta4 = net.addStation('sta4', mac = '00:00:00:00:00:04', ip = '10.0.0.4/8',
-    position = '50,40,0')
+    position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
     cl = net.addController('cl', controller=Controller)
 
     #Takes 5 minutes before to proceed
@@ -35,20 +35,22 @@ def topology():
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
-    time.sleep(90)
+    #time.sleep(90)
 
     net.plotGraph(max_x=60, max_y=60)
 
     info("*** Enabling Association control (AP)\n")
-    net.setAssociationCtrl('ssf')
+    
+    net.setAssociationCtrl(ac='ssf')
+    net.auto_association()
 
 
     info("*** Creating links and associations")
     net.addLink(ap1, ap2)
-    net.addLink(ap1, sta1)
-    net.addLink(ap2, sta2)
-    net.addLink(ap1,sta3)
-    net.addLink(ap2,sta4)
+    #net.addLink(ap1, sta1)
+    #net.addLink(ap2, sta2)
+    #net.addLink(ap1,sta3)
+    #net.addLink(ap2,sta4)
 
     info("*** Starting network\n")
     net.build()
