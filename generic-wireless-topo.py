8249043b1c8b31e41fb2f3cb4e8addc2b4457e0f
RugaCoder <fruganyumisa@gmail.com>
2019-10-19 02:50:57 -0400

    Added propagation models

--- generic-wireless-topo.py
+++ generic-wireless-topo.py
@@ -6,35 +6,38 @@ from mininet.log import setLogLevel, info
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.node import OVSKernelAP
 from mn_wifi.cli import CLI_wifi
+from mn_wifi.propagationModels import propagationModel
 
 
 def topology():
 
     print("Welcome to mininet-Wifi")
-    net = Mininet_wifi(controller = Controller, accessPoint =OVSKernelAP,allAutoAssociation=True)
+    net = Mininet_wifi(controller = Controller, accessPoint =OVSKernelAP,allAutoAssociation=True,fading_coefficient=3)
 
     info("*** Creating nodes\n")
     ap1 = net.addAccessPoint('ap1', ssid= 'iGrid-ap1', mode = 'g', channel = '1',
-    position = '10,30,0', range = '20')
+    model='DI524', position = '10,30,0', range = '20')
     ap2 = net.addAccessPoint('ap2', ssid= 'iGrid-ap2', mode = 'g', channel = '6',
-    position = '40,30,0', range = '20')
+    model='DI524',position = '40,30,0', range = '20')
     sta1 = net.addStation('sta1', mac = '00:00:00:00:00:01', ip = '10.0.0.1/8',
-    position = '10,20,0',active_scan=1,scan_freq="2412 2437 2462")
+    antennaHeight='1', antennaGain='5',position = '10,20,0',active_scan=1,scan_freq="2412 2437 2462")
     sta2 = net.addStation('sta2', mac = '00:00:00:00:00:02', ip = '10.0.0.2/8',
-    position = '28,30,0',active_scan=1,scan_freq="2412 2437 2462")
+    antennaHeight='1', antennaGain='5',position = '28,30,0',active_scan=1,scan_freq="2412 2437 2462")
     sta3 = net.addStation('sta3', mac = '00:00:00:00:00:03', ip = '10.0.0.3/8',
-    position = '10,40,0',active_scan=1,scan_freq="2412 2437 2462")
+    antennaHeight='1', antennaGain='5',  position = '10,40,0',active_scan=1,scan_freq="2412 2437 2462")
     sta4 = net.addStation('sta4', mac = '00:00:00:00:00:04', ip = '10.0.0.4/8',
-    position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+    antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
     cl = net.addController('cl', controller=Controller)
 
     #Takes 5 minutes before to proceed
     #time.sleep(40)
+    net.setPropagationModel(model="longDistance", exp=4)
 
 
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
+    
     #time.sleep(90)
 
     net.plotGraph(max_x=60, max_y=60)
